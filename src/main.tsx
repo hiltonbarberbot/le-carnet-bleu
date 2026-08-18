8af876c6a83431e24b8cea2a24c1d75814a5f7d5
James Hilton-Barber <jameshiltonbarber@gmail.com>
2026-08-18 17:42:04 +0200

    Apply classified paper theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './ui/App'
 import './ui/style.css'
+import './ui/aesthetic/classified.css'
 import { productNaming } from './product/naming'
 
 document.title = productNaming.documentTitle
